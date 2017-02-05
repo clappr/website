b98c07094ad84775bb483afe8dc2b80c7fd7fa9a
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2017-02-05 20:49:49 -0200

    feat(clappr-website): add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: path.resolve(__dirname, '/main.js'),
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require('./package.json').version)
+    })
+  ],
+  module: {
+   loaders: [
+     {
+       test: /\.css$/,
+       loader: 'style!css',
+       include: path.resolve(__dirname, 'stylesheets')
+     },
+     {
+       test: /\.html/, loader: 'html?minimize=false'
+     }
+  ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'clappr-website.js'
+  }
+}
